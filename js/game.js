bba67176b38c3936c8fc4ac6345e772cf2978407
Ivo Silva <silvaivosoares@gmail.com>
2022-04-11 11:16:05 +0100

    Added collision detection for pathing and separate logic for item grabbing

--- js/game.js
+++ js/game.js
@@ -6,12 +6,18 @@ class Game {
     this.drawDomElement = drawDomElement;
     this.selectedLevel = levelOne;
     this.drawnLevel = drawnLevel;
+    this.keysGrabbed = 0;
   }
 
-  // criar arrray com o nível desenhado. Cada elemento do array tem o DomElement específico daquela posição
-  // Quando o jogador carrega na tecla para seguir numa direção, verificar se naquela direção, 40px adiante
-  // há algum elemento. Se houver, não avança.
-  // Vai ser preciso fazer um forEach dentro da detectCollision para determinar qual objeto está logo ao lado do jogador
+  start() {
+    this.heysGrabbed = 0;
+    this.levelDesigner(this.selectedLevel);
+
+    let intervalId = setInterval(() => {
+
+    }, 10);
+  
+  }
 
   levelDesigner(selectedLevel) {
     for (let row = 0; row < selectedLevel.length; row++) {
@@ -23,7 +29,7 @@ class Game {
             path.positionX = column * 80;
             path.positionY = row * 80;
             this.drawDomElement(path);
-            drawnLevel[row].splice(column,0,path);
+            drawnLevel[row].splice(column, 0, path);
             break;
           case 1:
             const wall = new Wall();
@@ -31,16 +37,16 @@ class Game {
             wall.positionX = column * 80;
             wall.positionY = row * 80;
             drawDomElement(wall);
-            drawnLevel[row].splice(column,0,wall);
+            drawnLevel[row].splice(column, 0, wall);
             break;
           case 2:
-            const enemy = new Enemy();
-            enemy.domElement = createDomElement("enemy");
-            enemy.positionX = column * 80;
-            enemy.positionY = row * 80;
-            drawDomElement(enemy);
+            const key = new Key();
+            key.domElement = createDomElement("key");
+            key.positionX = column * 80;
+            key.positionY = row * 80;
+            drawDomElement(key);
             // Mudar futuramente para em vez de wall ser o objeto enemy
-            drawnLevel[row].splice(column,0,enemy);
+            drawnLevel[row].splice(column, 0, key);
             break;
           case 3:
             this.player = new Player();
@@ -48,7 +54,7 @@ class Game {
             this.player.positionX = column * 80;
             this.player.positionY = row * 80;
             drawDomElement(this.player);
-            drawnLevel[row].splice(column,0,this.player);
+            drawnLevel[row].splice(column, 0, this.player);
             break;
           case 4:
             const exit = new Exit();
@@ -57,65 +63,96 @@ class Game {
             exit.positionY = row * 80;
             drawDomElement(exit);
             // Mudar futuramente para em vez de wall ser o objeto Exit
-            drawnLevel[row].splice(column,0,exit);
+            drawnLevel[row].splice(column, 0, exit);
             break;
         }
       }
     }
   }
 
-  start() {
-    this.levelDesigner(this.selectedLevel);
+ 
+
+  pathingCollision(selectedLevel, direction) {
+    /* Get player coordinates based on the level grid
+    instead of the dom position */
+    let x = this.player.positionX / 80;
+    let y = this.player.positionY / 80;
+    switch (direction) {
+      case "left":
+        switch (selectedLevel[y][x - 1]){
+          case 1: return false;
+          case 2:
+            this.grabKey(this.drawnLevel[y][x - 1], this.drawnLevel);
+            this.drawnLevel[y][x - 1] = 0;
+            return true;
+          default: return true;
+        }
+      case "right":
+        switch (selectedLevel[y][x + 1]){
+          case 1: return false;
+          case 2:
+            this.grabKey(this.drawnLevel[y][x + 1], this.drawnLevel);
+            this.drawnLevel[y][x + 1] = 0;
+            return true;
+          default: return true;
+        }
+      case "up":
+        switch (selectedLevel[y - 1][x]){
+          case 1: return false;
+          case 2:
+            this.grabKey(this.drawnLevel[y - 1][x], this.drawnLevel);
+            this.drawnLevel[y - 1][x] = 0;
+            return true;
+          default: return true;
+        }
+      case "down":
+        switch (selectedLevel[y + 1][x]){
+          case 1: return false;
+          case 2:
+            this.grabKey(this.drawnLevel[y + 1][x], this.drawnLevel);
+            this.drawnLevel[y + 1][x] = 0;
+            return true;
+          default: return true;
+        }
+    }
   }
 
-  detectCollision(entity, drawnLevel) {
-    console.log(drawnLevel);
-    for (let row = 0; row < drawnLevel.length; row++) {
-      for (let column = 0; column < drawnLevel.length; column++) {
-        if (
-          entity.positionX <
-            drawnLevel[row][column].positionX + drawnLevel[row][column].width &&
-          entity.positionX + entity.width > drawnLevel[row][column].positionX &&
-          entity.positionY <
-            drawnLevel[row][column].positionY +
-              drawnLevel[row][column].height &&
-          entity.height + entity.positionY >
-            drawnLevel[row][column].positionY &&
-          drawnLevel[row][column].domElement.className === "wall"
-        ) {
-          return true;
-        }
-      }
-    }
+  enemyCollision(){
+
+  }
+
+  itemCollision(){
+
+  }
+
+  grabKey(key, drawnLevel){
+    key.domElement.remove();
+    this.keysGrabbed ++;
   }
 
   movePlayer(direction) {
     switch (direction) {
       case "left":
-        if (!this.detectCollision(this.player, this.drawnLevel)) {
+        if (this.pathingCollision(this.selectedLevel, "left")) {
           this.player.moveLeft();
         }
         break;
       case "right":
-        if (!this.detectCollision(this.player, this.drawnLevel)) {
+        if (this.pathingCollision(this.selectedLevel, "right")) {
           this.player.moveRight();
         }
         break;
       case "up":
-        if (!this.detectCollision(this.player, this.drawnLevel)) {
+        if (this.pathingCollision(this.selectedLevel, "up")) {
           this.player.moveUp();
         }
         break;
       case "down":
-        if (!this.detectCollision(this.player, this.drawnLevel)) {
+        if (this.pathingCollision(this.selectedLevel, "down")) {
           this.player.moveDown();
         }
         break;
     }
-
-    // Each time the player moves, the element is redrawn
-    // Calls the drawElement function and says it's the player
-    // that is moving.
     this.drawDomElement(this.player);
   }
 }
@@ -131,26 +168,26 @@ class Wall {
 }
 
 class Path {
-    constructor() {
-      this.positionX = 1;
-      this.positionY = 1;
-      this.height = 80;
-      this.width = 80;
-      this.domElement = 0;
-    }
+  constructor() {
+    this.positionX = 1;
+    this.positionY = 1;
+    this.height = 80;
+    this.width = 80;
+    this.domElement = 0;
   }
-
-class Exit {
-    constructor() {
-      this.positionX = 1;
-      this.positionY = 1;
-      this.height = 80;
-      this.width = 80;
-      this.domElement = 0;
-    }
 }
 
-class Enemy {
+class Exit {
+  constructor() {
+    this.positionX = 1;
+    this.positionY = 1;
+    this.height = 80;
+    this.width = 80;
+    this.domElement = 0;
+  }
+}
+
+class Key {
   constructor() {
     this.positionX = 1;
     this.positionY = 1;
